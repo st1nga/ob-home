0458e74e9ca2f65961089b0f0bb760891b1321e0
st1nga <stinga@wolf-rock.com>
2020-03-23 21:00:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,6 @@ pip install openob
 read -p "Enter password for coastfm: " passwd
 useradd -m coastfm
 echo coastfm:${passwd} | chpasswd
-cd /tmp
 echo "Adding alias's to /etc/profile"
 echo 'alias ll="ls -l --color=auto"' >> /etc/profile
 . /etc/profile
