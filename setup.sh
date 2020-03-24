337cacc0b25b01afe0efd16223e5af918260446a
st1nga <stinga@wolf-rock.com>
2020-03-24 13:14:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,27 +16,38 @@ apt -y install python-gst-1.0
 apt -y install python-redis
 apt -y install python-gi
 apt -y install python-setuptools
+apt -y install unzip
+apt -y install net-tools
 echo "Installing openOB"
 pip install openob
 read -p "Enter password for coastfm: " passwd
 useradd -m coastfm
 echo coastfm:${passwd} | chpasswd
-echo "Adding alias's to /etc/profile"
-echo 'alias ll="ls -l --color=auto"' >> /etc/profile
-. /etc/profile
 read -p "Insert usb drive, press <enter> when done " yn
 echo "Setting up openvpn"
-mv client.conf /etc/openvpn/
-mv cacert.pem /etc/openvpn/
-mv obsetup.pem /etc/openvpn/
-mv obsetup.key /etc/openvpn/
+usb_media=/root
+unzip obsetup-TO-IPFire.zip
+mv ${usb_media}/client.conf /etc/openvpn/
+mv ${usb_media}/cacert.pem /etc/openvpn/
+mv ${usb_media}/obsetup.pem /etc/openvpn/
+mv ${usb_media}/obsetup.key /etc/openvpn/
 echo "Restarting openvpn"
 systemctl restart openvpn
 read -p "Irriversable changes about to be made... COntinue (y)n ": yn
 if [ "$yn" != "y" ]
 then
-  return
+  exit
 fi
+
+echo "Adding alias's to /etc/profile"
+grep "alias ll" /etc/profile > /dev/null
+if [ $? = 1 ]
+then
+  echo 'alias ll="ls -l --color=auto"' >> /etc/profile
+  echo 'alias r='fc -e -' ='fc -e -'
+fi
+. /etc/profile
+
 cat /proc/self/net/dev
 read -p "Enter the 2nd network interface: " interface_2
 echo "#pci card interface used for radiodj pc etc al" >> /etc/network/interfaces
